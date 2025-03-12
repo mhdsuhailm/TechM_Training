fbd49d32f6285fed5e6e0f944a77cb79eb11df0b
Muhammad suhail <mhdsuhailmohaideen786@gmail.com>
2025-03-12 13:19:53 +0500

    Removed 09_Angular_Task folder

--- 09_Angular_Task/src/app/pipes/filter-books.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterBooksPipe } from './filter-books.pipe';
-
-describe('FilterBooksPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterBooksPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
